525ecdb1fc41afbe994c720a1d4c49bc9f4baac7
Alexander Repeta <alexander.repeta@gmail.com>
2023-01-09 11:39:03 +0200

    change base path handling

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite';
 
-const BASE_PUBLIC_PATH_DEV = '/';
-const BASE_PUBLIC_PATH_PROD = '/vanilla-app-template/';
-
 export default defineConfig({
   root: 'src',
-  base:
-    process.env.NODE_ENV === 'development'
-      ? BASE_PUBLIC_PATH_DEV
-      : BASE_PUBLIC_PATH_PROD,
 });
