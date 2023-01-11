dc4df7d1f4c3244dfecdece0bf688e114d1a3745
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-01-11 15:16:25 +0300

    add partials to config
    - add flag --open to script "dev"
    - update config
    - installed Dependencies:
      path
      vite-plugin-handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
-
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
   },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, './src/partials'),
+    }),
+  ],
 });
