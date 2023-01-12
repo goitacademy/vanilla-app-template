d3008128b89b815be62d46338e5d512a845f5a2a
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-01-12 02:04:13 +0200

    add config for html partials

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
   },
+  plugins: [injectHTML()],
 });
