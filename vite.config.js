4672173273af34d6bf35eaa0b846e5dc7154c32d
Alexander Repeta <alexander.repeta@gmail.com>
2023-09-28 18:38:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
+   define: {
+    global: {},
+  },
   root: 'src',
   build: {
     rollupOptions: {
