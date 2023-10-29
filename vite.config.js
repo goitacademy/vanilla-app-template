a2a1daae657e0fa58e666c91531f9b2f4e7aabae
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-10-29 22:33:12 +0300

    update config and deploy

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,34 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  define: {
-    _global: {},
-  },
-  root: 'src',
-  build: {
-    rollupOptions: {
-      input: glob.sync('./src/*.html'),
-    },
-    outDir: '../dist',
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      define: {
+        global: {},
+      },
+      root: 'src',
+      build: {
+        rollupOptions: {
+          input: glob.sync('./src/*.html'),
+        },
+        outDir: '../dist',
+      },
+      plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    };
+  } else {
+    return {
+      define: {
+        _global: {},
+      },
+      root: 'src',
+      build: {
+        rollupOptions: {
+          input: glob.sync('./src/*.html'),
+        },
+        outDir: '../dist',
+      },
+      plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    };
+  }
 });
