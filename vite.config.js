478ea0ac6545933b5abc53222e8499afacf55959
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-04-21 19:19:28 +0300

    redesign pages, update: config and styles

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
+import glob from 'fast-glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig(({ command }) => {
   return {
@@ -25,6 +28,27 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      ViteImageOptimizer({
+        png: {
+          quality: 86,
+        },
+        jpeg: {
+          quality: 86,
+        },
+        jpg: {
+          quality: 86,
+        },
+      }),
+      {
+        ...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
+          destination: './src/img/webp/',
+          plugins: [imageminWebp({ quality: 86 })],
+        }),
+        apply: 'serve',
+      },
+    ],
   };
 });
