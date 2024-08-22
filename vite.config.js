df7668cabe1ac4567a7a2270a16524aadba4c3e2
Alexander Repeta <alexander.repeta@gmail.com>
2024-08-22 11:53:33 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
+import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
@@ -12,9 +12,8 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/*.html', { absolute: true }).matches,
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
